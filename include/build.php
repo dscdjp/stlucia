542024e134b45fa79e3b891a3c5fd81ecf92a5ee
Fundec <fundec.inc@gmai.com>
2018-03-01 03:53:19 +0900

    社会復帰施設　ナビ　作成

--- include/build.php
+++ include/build.php
@@ -1,40 +1,36 @@
-<div class="build">
+<div class="build-link">
 	<div class="container">
 		<h3><span>施設一覧</span></h3>
 		<div class="row">
 			<div class="col-md-3">
-				<div class="build-item">
+				<div class="build-link-item">
 					<a href="<?=esc_url(home_url('/lupinus/'))?>"<?=$current=='lupinus'?' class="active"':''?>>
-						<h4 class="build-item-title">グループホーム　ルピナス</h4>
-						<p class="build-item-thum"><img src="<?=get_template_directory_uri()?>/dest/img/clover/list_lupinus_thum.png"></p>
-						<p class="build-item-text">共同生活を通じて、地域社会の中で自立した生活を実現していくことを目的とした住居施設です。</p>
+						<img src="<?=get_template_directory_uri()?>/dest/img/clover/list_lupinus_thum.png">
+						<h4 class="build-link-item-title">グループホーム<br>ルピナス</h4>
 					</a>
 				</div>
 			</div>
 			<div class="col-md-3">
-				<div class="build-item">
+				<div class="build-link-item">
 					<a href="<?=esc_url(home_url('/clover/'))?>"<?=$current=='clover'?' class="active"':''?>>
-						<h4 class="build-item-title">訪問介護ステーション　クローバー</h4>
-						<p class="build-item-thum"><img src="<?=get_template_directory_uri()?>/dest/img/clover/list_clover_thum.png"></p>
-						<p class="build-item-text">ご自宅等で安心して生活が送れるよう、医療スタッフがご自宅に伺い「看護」や｢リハビリ｣を提供します。</p>
+						<img src="<?=get_template_directory_uri()?>/dest/img/clover/list_clover_thum.png">
+						<h4 class="build-link-item-title">訪問介護ステーション<br>クローバー</h4>
 					</a>
 				</div>
 			</div>
 			<div class="col-md-3">
-				<div class="build-item">
+				<div class="build-link-item">
 					<a href="<?=esc_url(home_url('/daycare/'))?>"<?=$current=='daycare'?' class="active"':''?>>
-						<h4 class="build-item-title">精神科デイケア、デイナイトケア、ショートケア</h4>
-						<p class="build-item-thum"><img src="<?=get_template_directory_uri()?>/dest/img/clover/list_daycare_thum.png"></p>
-						<p class="build-item-text">多種多様なプログラムを通して、社会生活を送る上で必要な知識や技術を身に付け、自身を回復し、より健康的な生活が送れることを目指します。</p>
+						<img src="<?=get_template_directory_uri()?>/dest/img/clover/list_daycare_thum.png">
+						<h4 class="build-link-item-title">精神科デイケア、デイナイトケア、ショートケア</h4>
 					</a>
 				</div>
 			</div>
 			<div class="col-md-3">
-				<div class="build-item">
+				<div class="build-link-item">
 					<a href="<?=esc_url(home_url('/suzuran/'))?>"><?=$current=='suzuran'?' class="active"':''?>
-						<h4 class="build-item-title">重度認知症患者デイケア　すずらん</h4>
-						<p class="build-item-thum"><img src="<?=get_template_directory_uri()?>/dest/img/clover/list_suzuran_thum.png"></p>
-						<p class="build-item-text">認知症の進行を予防し、認知症の行動・心理症状（不穏、興奮、介護拒否など）の緩和と精神的な安定を図る通所施設です。利用者様に合った「治療・リハビリ・介護」を提供します。</p>
+						<img src="<?=get_template_directory_uri()?>/dest/img/clover/list_suzuran_thum.png">
+						<h4 class="build-link-item-title">重度認知症患者デイケア<br>すずらん</h4>
 					</a>
 				</div>
 			</div>
